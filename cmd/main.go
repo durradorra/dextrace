0f6f3974b5a2a392a8e71b1187d3bfe4c47b947f
brkss <berkassebrahim@gmail.com>
2025-03-25 17:23:27 +0000

    init server : load env user creds

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+type SibionicUser struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
 
 
 func main() {
 	
+	var user SibionicUser
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	user.Email = os.Getenv("USER_EMAIL")
+	user.Password = os.Getenv("USER_PASSWORD")
+
+	fmt.Println(user)
+
 	r := gin.Default()
 
 	
