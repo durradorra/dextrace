f549d264c241353fad24fff2f57aa5c697492816
brkss <berkassebrahim@gmail.com>
2025-03-25 17:16:31 +0000

    ignore .env

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	
+	r := gin.Default()
+
+	
+	r.GET("/data", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	
+	r.Run(":8080")
 }
